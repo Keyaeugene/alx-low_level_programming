689224b67cc065910dc651b129b2f847ef431d9b
keyaeugene <keyaeugene@gmail.com>
2023-09-07 05:57:40 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,32 +10,38 @@
  * Return: pointer to the resulting string
  */
 
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2;
-	char *result;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-	s1 = "";
-	if (s2 == NULL)
-	s2 = "";
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (n >= len2)
-	n = len2;
+	if (!s)
+		return (NULL);
 
-	result = (char *)malloc(len1 + n + 1);
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-	if (result == NULL)
-	return (NULL);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	strcpy(result, s1);
-	strncat(result, s2, n);
+	s[i] = '\0';
 
-	return (result);
+	return (s);
 }
 
